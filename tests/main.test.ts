bce64af03ac2e917dac02e2b57aa40aeab4cf127
shadieid <shadi.eid@live.com>
2024-08-21 13:32:18 +0300

    Mocking data

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,9 +1,3 @@
 describe("group", () => {
-  it("should", async () => {
-    const response = await fetch("/categories");
-    const data = await response.json();
-    console.log(data);
-
-    expect(1).toBeTruthy();
-  });
+  it("should", async () => {});
 });
